e5406a1410998275bfd495ef00de33d9c277ec9b
ThanhVo15 <thanhvm21416c@st.uel.edu.vn>
2024-05-05 01:13:24 +0700

    Preprocessing and save file to sqlite

---
+++ MLBA/Final Project/Data/Preprocessing Data/generations.sql
@@ -0,0 +1,74 @@
+CREATE TABLE "generations" (
+"birth_year" INTEGER,
+  "generation" TEXT
+);
+INSERT INTO generations VALUES ('1946', 'Baby Boomers');
+INSERT INTO generations VALUES ('1947', 'Baby Boomers');
+INSERT INTO generations VALUES ('1948', 'Baby Boomers');
+INSERT INTO generations VALUES ('1949', 'Baby Boomers');
+INSERT INTO generations VALUES ('1950', 'Baby Boomers');
+INSERT INTO generations VALUES ('1951', 'Baby Boomers');
+INSERT INTO generations VALUES ('1952', 'Baby Boomers');
+INSERT INTO generations VALUES ('1953', 'Baby Boomers');
+INSERT INTO generations VALUES ('1954', 'Baby Boomers');
+INSERT INTO generations VALUES ('1955', 'Baby Boomers');
+INSERT INTO generations VALUES ('1956', 'Baby Boomers');
+INSERT INTO generations VALUES ('1957', 'Baby Boomers');
+INSERT INTO generations VALUES ('1958', 'Baby Boomers');
+INSERT INTO generations VALUES ('1959', 'Baby Boomers');
+INSERT INTO generations VALUES ('1960', 'Baby Boomers');
+INSERT INTO generations VALUES ('1961', 'Baby Boomers');
+INSERT INTO generations VALUES ('1962', 'Baby Boomers');
+INSERT INTO generations VALUES ('1963', 'Baby Boomers');
+INSERT INTO generations VALUES ('1964', 'Baby Boomers');
+INSERT INTO generations VALUES ('1965', 'Gen X');
+INSERT INTO generations VALUES ('1966', 'Gen X');
+INSERT INTO generations VALUES ('1967', 'Gen X');
+INSERT INTO generations VALUES ('1968', 'Gen X');
+INSERT INTO generations VALUES ('1969', 'Gen X');
+INSERT INTO generations VALUES ('1970', 'Gen X');
+INSERT INTO generations VALUES ('1971', 'Gen X');
+INSERT INTO generations VALUES ('1972', 'Gen X');
+INSERT INTO generations VALUES ('1973', 'Gen X');
+INSERT INTO generations VALUES ('1974', 'Gen X');
+INSERT INTO generations VALUES ('1975', 'Gen X');
+INSERT INTO generations VALUES ('1976', 'Gen X');
+INSERT INTO generations VALUES ('1977', 'Gen X');
+INSERT INTO generations VALUES ('1978', 'Gen X');
+INSERT INTO generations VALUES ('1979', 'Gen X');
+INSERT INTO generations VALUES ('1980', 'Older Millennials');
+INSERT INTO generations VALUES ('1981', 'Older Millennials');
+INSERT INTO generations VALUES ('1982', 'Older Millennials');
+INSERT INTO generations VALUES ('1983', 'Older Millennials');
+INSERT INTO generations VALUES ('1984', 'Older Millennials');
+INSERT INTO generations VALUES ('1985', 'Older Millennials');
+INSERT INTO generations VALUES ('1986', 'Older Millennials');
+INSERT INTO generations VALUES ('1987', 'Older Millennials');
+INSERT INTO generations VALUES ('1988', 'Older Millennials');
+INSERT INTO generations VALUES ('1989', 'Older Millennials');
+INSERT INTO generations VALUES ('1990', 'Younger Millennials');
+INSERT INTO generations VALUES ('1991', 'Younger Millennials');
+INSERT INTO generations VALUES ('1992', 'Younger Millennials');
+INSERT INTO generations VALUES ('1993', 'Younger Millennials');
+INSERT INTO generations VALUES ('1994', 'Younger Millennials');
+INSERT INTO generations VALUES ('1995', 'Gen Z');
+INSERT INTO generations VALUES ('1996', 'Gen Z');
+INSERT INTO generations VALUES ('1997', 'Gen Z');
+INSERT INTO generations VALUES ('1998', 'Gen Z');
+INSERT INTO generations VALUES ('1999', 'Gen Z');
+INSERT INTO generations VALUES ('2000', 'Gen Z');
+INSERT INTO generations VALUES ('2001', 'Gen Z');
+INSERT INTO generations VALUES ('2002', 'Gen Z');
+INSERT INTO generations VALUES ('2003', 'Gen Z');
+INSERT INTO generations VALUES ('2004', 'Gen Z');
+INSERT INTO generations VALUES ('2005', 'Gen Z');
+INSERT INTO generations VALUES ('2006', 'Gen Z');
+INSERT INTO generations VALUES ('2007', 'Gen Z');
+INSERT INTO generations VALUES ('2008', 'Gen Z');
+INSERT INTO generations VALUES ('2009', 'Gen Z');
+INSERT INTO generations VALUES ('2010', 'Gen Z');
+INSERT INTO generations VALUES ('2011', 'Gen Z');
+INSERT INTO generations VALUES ('2012', 'Gen Z');
+INSERT INTO generations VALUES ('2013', 'Gen Z');
+INSERT INTO generations VALUES ('2014', 'Gen Z');
+INSERT INTO generations VALUES ('2015', 'Gen Z');
---
+++ MLBA/Final Project/Data/Preprocessing Data/sales_targets.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "sales_targets" (
+"sales_outlet_id" INTEGER,
+  "beans_goal" INTEGER,
+  "beverage_goal" INTEGER,
+  "food_goal" INTEGER,
+  "merchandise _goal" INTEGER,
+  "total_goal" INTEGER,
+  "Date_ID" INTEGER,
+  "Year_ID" INTEGER
+);
+INSERT INTO sales_targets VALUES ('3', '720', '13500', '3420', '360', '18000', '4', '2019');
+INSERT INTO sales_targets VALUES ('4', '720', '13500', '3420', '360', '18000', '4', '2019');
+INSERT INTO sales_targets VALUES ('5', '1000', '18750', '4750', '500', '25000', '4', '2019');
+INSERT INTO sales_targets VALUES ('6', '720', '13500', '3420', '360', '18000', '4', '2019');
+INSERT INTO sales_targets VALUES ('7', '720', '13500', '3420', '360', '18000', '4', '2019');
+INSERT INTO sales_targets VALUES ('8', '900', '16875', '4275', '450', '22500', '4', '2019');
+INSERT INTO sales_targets VALUES ('9', '720', '13500', '3420', '360', '18000', '4', '2019');
+INSERT INTO sales_targets VALUES ('10', '720', '13500', '3420', '360', '18000', '4', '2019');
