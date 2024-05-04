eabea61f99f9988119662eb732477f64ec807fa4
ThanhVo15 <thanhvm21416c@st.uel.edu.vn>
2024-05-05 01:20:10 +0700

    csv to sqlite

--- MLBA/Final Project/Data/Preprocessing Data/staff.sql
+++ MLBA/Final Project/Data/Preprocessing Data/staff.sql
@@ -3,61 +3,61 @@ CREATE TABLE "staff" (
   "first_name" TEXT,
   "last_name" TEXT,
   "position" TEXT,
-  "start_date" TEXT,
+  "start_date" TIMESTAMP,
   "location" TEXT
 );
-INSERT INTO staff VALUES ('1', 'Sue', 'Tindale', 'CFO', '8/3/2001', 'HQ');
-INSERT INTO staff VALUES ('2', 'Ian', 'Tindale', 'CEO', '8/3/2001', 'HQ');
-INSERT INTO staff VALUES ('3', 'Marny', 'Hermione', 'Roaster', '10/24/2007', 'WH');
-INSERT INTO staff VALUES ('4', 'Chelsea', 'Claudia', 'Roaster', '7/3/2003', 'WH');
-INSERT INTO staff VALUES ('5', 'Alec', 'Isadora', 'Roaster', '4/2/2008', 'WH');
-INSERT INTO staff VALUES ('6', 'Xena', 'Rahim', 'Store Manager', '7/24/2016', '3');
-INSERT INTO staff VALUES ('7', 'Kelsey', 'Cameron', 'Coffee Wrangler', '10/18/2003', '3');
-INSERT INTO staff VALUES ('8', 'Hamilton', 'Emi', 'Coffee Wrangler', '2/9/2005', '3');
-INSERT INTO staff VALUES ('9', 'Caldwell', 'Veda', 'Coffee Wrangler', '9/9/2013', '3');
-INSERT INTO staff VALUES ('10', 'Ima', 'Winifred', 'Coffee Wrangler', '12/10/2016', '3');
-INSERT INTO staff VALUES ('11', 'Ruth', 'Leslie', 'Store Manager', '6/17/2009', '4');
-INSERT INTO staff VALUES ('12', 'Britanni', 'Jorden', 'Coffee Wrangler', '3/25/2006', '4');
-INSERT INTO staff VALUES ('13', 'Berk', 'Derek', 'Coffee Wrangler', '12/11/2009', '4');
-INSERT INTO staff VALUES ('14', 'Damon', 'Sasha', 'Coffee Wrangler', '6/5/2010', '4');
-INSERT INTO staff VALUES ('15', 'Remedios', 'Mari', 'Coffee Wrangler', '5/9/2014', '4');
-INSERT INTO staff VALUES ('16', 'Reed', 'Eve', 'Store Manager', '3/30/2006', '5');
-INSERT INTO staff VALUES ('17', 'Quail', 'Octavia', 'Coffee Wrangler', '12/5/2014', '5');
-INSERT INTO staff VALUES ('18', 'Ezekiel', 'Rashad', 'Coffee Wrangler', '11/13/2005', '5');
-INSERT INTO staff VALUES ('19', 'Peter', 'Paloma', 'Coffee Wrangler', '3/12/2014', '5');
-INSERT INTO staff VALUES ('20', 'Ronan', 'Magee', 'Coffee Wrangler', '2/13/2002', '5');
-INSERT INTO staff VALUES ('21', 'Melodie', 'Mercedes', 'Store Manager', '9/29/2018', '6');
-INSERT INTO staff VALUES ('22', 'Marny', 'Dennis', 'Coffee Wrangler', '3/3/2014', '6');
-INSERT INTO staff VALUES ('23', 'Blythe', 'Arsenio', 'Coffee Wrangler', '11/22/2018', '6');
-INSERT INTO staff VALUES ('24', 'Garrett', 'Doris', 'Coffee Wrangler', '1/27/2007', '6');
-INSERT INTO staff VALUES ('25', 'Aline', 'Melanie', 'Coffee Wrangler', '3/14/2017', '6');
-INSERT INTO staff VALUES ('26', 'Joelle', 'Christen', 'Store Manager', '11/11/2013', '7');
-INSERT INTO staff VALUES ('27', 'Ainsley', 'Evelyn', 'Coffee Wrangler', '7/31/2003', '7');
-INSERT INTO staff VALUES ('28', 'Joseph', 'Byron', 'Coffee Wrangler', '5/17/2014', '7');
-INSERT INTO staff VALUES ('29', 'Orson', 'Benedict', 'Coffee Wrangler', '7/26/2016', '7');
-INSERT INTO staff VALUES ('30', 'Amela', 'Chadwick', 'Coffee Wrangler', '9/17/2005', '7');
-INSERT INTO staff VALUES ('31', 'Dawn', 'Anthony', 'Store Manager', '7/2/2009', '8');
-INSERT INTO staff VALUES ('32', 'Alisa', 'Lysandra', 'Coffee Wrangler', '2/2/2005', '8');
-INSERT INTO staff VALUES ('33', 'Cairo', 'Vaughan', 'Coffee Wrangler', '2/18/2015', '8');
-INSERT INTO staff VALUES ('34', 'Yasir', 'Lillith', 'Coffee Wrangler', '2/16/2016', '8');
-INSERT INTO staff VALUES ('35', 'Xavier', 'Zachary', 'Coffee Wrangler', '1/7/2014', '8');
-INSERT INTO staff VALUES ('36', 'Anthony', 'Kaitlin', 'Store Manager', '4/19/2004', '9');
-INSERT INTO staff VALUES ('37', 'Hop', 'Bianca', 'Coffee Wrangler', '2/11/2015', '9');
-INSERT INTO staff VALUES ('38', 'Ezekiel', 'Bertha', 'Coffee Wrangler', '10/15/2008', '9');
-INSERT INTO staff VALUES ('39', 'Vance', 'Samuel', 'Coffee Wrangler', '6/9/2016', '9');
-INSERT INTO staff VALUES ('40', 'Brent', 'Herman', 'Coffee Wrangler', '10/25/2001', '9');
-INSERT INTO staff VALUES ('41', 'Adrian', 'Macon', 'Store Manager', '10/13/2001', '10');
-INSERT INTO staff VALUES ('42', 'Kylie', 'Candace', 'Coffee Wrangler', '1/30/2011', '10');
-INSERT INTO staff VALUES ('43', 'Tatum', 'Laurel', 'Coffee Wrangler', '1/31/2015', '10');
-INSERT INTO staff VALUES ('44', 'Tamekah', 'Maya', 'Coffee Wrangler', '5/17/2005', '10');
-INSERT INTO staff VALUES ('45', 'Pandora', 'Neville', 'Coffee Wrangler', '3/21/2019', '10');
-INSERT INTO staff VALUES ('46', 'Desiree', 'Anika', 'Store Manager', '6/15/2008', 'FL');
-INSERT INTO staff VALUES ('47', 'Hope', 'Sheila', 'Coffee Wrangler', '1/31/2003', 'FL');
-INSERT INTO staff VALUES ('48', 'Clare', 'Oscar', 'Coffee Wrangler', '2/21/2003', 'FL');
-INSERT INTO staff VALUES ('49', 'Gemma', 'Eaton', 'Coffee Wrangler', '5/4/2013', 'FL');
-INSERT INTO staff VALUES ('50', 'Dale', 'Joshua', 'Coffee Wrangler', '10/1/2015', 'FL');
-INSERT INTO staff VALUES ('51', 'Lawrence', 'Roberts', 'Store Manager', '8/3/2001', 'FL');
-INSERT INTO staff VALUES ('52', 'Melinda', 'Zeus', 'Coffee Wrangler', '8/17/2003', 'FL');
-INSERT INTO staff VALUES ('53', 'Sawyer', 'Kasper', 'Coffee Wrangler', '11/11/2007', 'FL');
-INSERT INTO staff VALUES ('54', 'Ezekiel', 'Griffin', 'Coffee Wrangler', '9/13/2005', 'FL');
-INSERT INTO staff VALUES ('55', 'Coby', 'Shelly', 'Coffee Wrangler', '11/17/2015', 'FL');
+INSERT INTO staff VALUES ('1', 'Sue', 'Tindale', 'CFO', '2001-08-03', 'HQ');
+INSERT INTO staff VALUES ('2', 'Ian', 'Tindale', 'CEO', '2001-08-03', 'HQ');
+INSERT INTO staff VALUES ('3', 'Marny', 'Hermione', 'Roaster', '2007-10-24', 'WH');
+INSERT INTO staff VALUES ('4', 'Chelsea', 'Claudia', 'Roaster', '2003-07-03', 'WH');
+INSERT INTO staff VALUES ('5', 'Alec', 'Isadora', 'Roaster', '2008-04-02', 'WH');
+INSERT INTO staff VALUES ('6', 'Xena', 'Rahim', 'Store Manager', '2016-07-24', '3');
+INSERT INTO staff VALUES ('7', 'Kelsey', 'Cameron', 'Coffee Wrangler', '2003-10-18', '3');
+INSERT INTO staff VALUES ('8', 'Hamilton', 'Emi', 'Coffee Wrangler', '2005-02-09', '3');
+INSERT INTO staff VALUES ('9', 'Caldwell', 'Veda', 'Coffee Wrangler', '2013-09-09', '3');
+INSERT INTO staff VALUES ('10', 'Ima', 'Winifred', 'Coffee Wrangler', '2016-12-10', '3');
+INSERT INTO staff VALUES ('11', 'Ruth', 'Leslie', 'Store Manager', '2009-06-17', '4');
+INSERT INTO staff VALUES ('12', 'Britanni', 'Jorden', 'Coffee Wrangler', '2006-03-25', '4');
+INSERT INTO staff VALUES ('13', 'Berk', 'Derek', 'Coffee Wrangler', '2009-12-11', '4');
+INSERT INTO staff VALUES ('14', 'Damon', 'Sasha', 'Coffee Wrangler', '2010-06-05', '4');
+INSERT INTO staff VALUES ('15', 'Remedios', 'Mari', 'Coffee Wrangler', '2014-05-09', '4');
+INSERT INTO staff VALUES ('16', 'Reed', 'Eve', 'Store Manager', '2006-03-30', '5');
+INSERT INTO staff VALUES ('17', 'Quail', 'Octavia', 'Coffee Wrangler', '2014-12-05', '5');
+INSERT INTO staff VALUES ('18', 'Ezekiel', 'Rashad', 'Coffee Wrangler', '2005-11-13', '5');
+INSERT INTO staff VALUES ('19', 'Peter', 'Paloma', 'Coffee Wrangler', '2014-03-12', '5');
+INSERT INTO staff VALUES ('20', 'Ronan', 'Magee', 'Coffee Wrangler', '2002-02-13', '5');
+INSERT INTO staff VALUES ('21', 'Melodie', 'Mercedes', 'Store Manager', '2018-09-29', '6');
+INSERT INTO staff VALUES ('22', 'Marny', 'Dennis', 'Coffee Wrangler', '2014-03-03', '6');
+INSERT INTO staff VALUES ('23', 'Blythe', 'Arsenio', 'Coffee Wrangler', '2018-11-22', '6');
+INSERT INTO staff VALUES ('24', 'Garrett', 'Doris', 'Coffee Wrangler', '2007-01-27', '6');
+INSERT INTO staff VALUES ('25', 'Aline', 'Melanie', 'Coffee Wrangler', '2017-03-14', '6');
+INSERT INTO staff VALUES ('26', 'Joelle', 'Christen', 'Store Manager', '2013-11-11', '7');
+INSERT INTO staff VALUES ('27', 'Ainsley', 'Evelyn', 'Coffee Wrangler', '2003-07-31', '7');
+INSERT INTO staff VALUES ('28', 'Joseph', 'Byron', 'Coffee Wrangler', '2014-05-17', '7');
+INSERT INTO staff VALUES ('29', 'Orson', 'Benedict', 'Coffee Wrangler', '2016-07-26', '7');
+INSERT INTO staff VALUES ('30', 'Amela', 'Chadwick', 'Coffee Wrangler', '2005-09-17', '7');
+INSERT INTO staff VALUES ('31', 'Dawn', 'Anthony', 'Store Manager', '2009-07-02', '8');
+INSERT INTO staff VALUES ('32', 'Alisa', 'Lysandra', 'Coffee Wrangler', '2005-02-02', '8');
+INSERT INTO staff VALUES ('33', 'Cairo', 'Vaughan', 'Coffee Wrangler', '2015-02-18', '8');
+INSERT INTO staff VALUES ('34', 'Yasir', 'Lillith', 'Coffee Wrangler', '2016-02-16', '8');
+INSERT INTO staff VALUES ('35', 'Xavier', 'Zachary', 'Coffee Wrangler', '2014-01-07', '8');
+INSERT INTO staff VALUES ('36', 'Anthony', 'Kaitlin', 'Store Manager', '2004-04-19', '9');
+INSERT INTO staff VALUES ('37', 'Hop', 'Bianca', 'Coffee Wrangler', '2015-02-11', '9');
+INSERT INTO staff VALUES ('38', 'Ezekiel', 'Bertha', 'Coffee Wrangler', '2008-10-15', '9');
+INSERT INTO staff VALUES ('39', 'Vance', 'Samuel', 'Coffee Wrangler', '2016-06-09', '9');
+INSERT INTO staff VALUES ('40', 'Brent', 'Herman', 'Coffee Wrangler', '2001-10-25', '9');
+INSERT INTO staff VALUES ('41', 'Adrian', 'Macon', 'Store Manager', '2001-10-13', '10');
+INSERT INTO staff VALUES ('42', 'Kylie', 'Candace', 'Coffee Wrangler', '2011-01-30', '10');
+INSERT INTO staff VALUES ('43', 'Tatum', 'Laurel', 'Coffee Wrangler', '2015-01-31', '10');
+INSERT INTO staff VALUES ('44', 'Tamekah', 'Maya', 'Coffee Wrangler', '2005-05-17', '10');
+INSERT INTO staff VALUES ('45', 'Pandora', 'Neville', 'Coffee Wrangler', '2019-03-21', '10');
+INSERT INTO staff VALUES ('46', 'Desiree', 'Anika', 'Store Manager', '2008-06-15', 'FL');
+INSERT INTO staff VALUES ('47', 'Hope', 'Sheila', 'Coffee Wrangler', '2003-01-31', 'FL');
+INSERT INTO staff VALUES ('48', 'Clare', 'Oscar', 'Coffee Wrangler', '2003-02-21', 'FL');
+INSERT INTO staff VALUES ('49', 'Gemma', 'Eaton', 'Coffee Wrangler', '2013-05-04', 'FL');
+INSERT INTO staff VALUES ('50', 'Dale', 'Joshua', 'Coffee Wrangler', '2015-10-01', 'FL');
+INSERT INTO staff VALUES ('51', 'Lawrence', 'Roberts', 'Store Manager', '2001-08-03', 'FL');
+INSERT INTO staff VALUES ('52', 'Melinda', 'Zeus', 'Coffee Wrangler', '2003-08-17', 'FL');
+INSERT INTO staff VALUES ('53', 'Sawyer', 'Kasper', 'Coffee Wrangler', '2007-11-11', 'FL');
+INSERT INTO staff VALUES ('54', 'Ezekiel', 'Griffin', 'Coffee Wrangler', '2005-09-13', 'FL');
+INSERT INTO staff VALUES ('55', 'Coby', 'Shelly', 'Coffee Wrangler', '2015-11-17', 'FL');
