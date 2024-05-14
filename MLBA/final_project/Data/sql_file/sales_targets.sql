6fba54587c3307880273f9446f6440b37b5a1f7a
ThanhVo15 <thanhvm21416c@st.uel.edu.vn>
2024-05-14 17:30:58 +0700

    hi

---
+++ MLBA/final_project/Data/sql_file/sales_targets.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sales_targets(
+   sales_outlet_id  INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,beans_goal       INTEGER  NOT NULL
+  ,beverage_goal    INTEGER  NOT NULL
+  ,food_goal        INTEGER  NOT NULL
+  ,merchandise_goal INTEGER  NOT NULL
+  ,total_goal       INTEGER  NOT NULL
+  ,Date_ID          INTEGER  NOT NULL
+  ,Year_ID          INTEGER  NOT NULL
+);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (3,720,13500,3420,360,18000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (4,720,13500,3420,360,18000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (5,1000,18750,4750,500,25000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (6,720,13500,3420,360,18000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (7,720,13500,3420,360,18000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (8,900,16875,4275,450,22500,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (9,720,13500,3420,360,18000,4,2019);
+INSERT INTO sales_targets(sales_outlet_id,beans_goal,beverage_goal,food_goal,merchandise_goal,total_goal,Date_ID,Year_ID) VALUES (10,720,13500,3420,360,18000,4,2019);
---
+++ MLBA/final_project/Data/sql_file/staff.sql
@@ -0,0 +1,63 @@
+CREATE TABLE staffs(
+   staff_id   INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,first_name VARCHAR(8) NOT NULL
+  ,last_name  VARCHAR(8) NOT NULL
+  ,position   VARCHAR(15) NOT NULL
+  ,start_date DATE  NOT NULL
+  ,location   VARCHAR(2) NOT NULL
+);
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (1,'Sue','Tindale','CFO','2001-08-03','HQ');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (2,'Ian','Tindale','CEO','2001-08-03','HQ');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (3,'Marny','Hermione','Roaster','2007-10-24','WH');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (4,'Chelsea','Claudia','Roaster','2003-07-03','WH');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (5,'Alec','Isadora','Roaster','2008-04-02','WH');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (6,'Xena','Rahim','Store Manager','2016-07-24','3');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (7,'Kelsey','Cameron','Coffee Wrangler','2003-10-18','3');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (8,'Hamilton','Emi','Coffee Wrangler','2005-02-09','3');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (9,'Caldwell','Veda','Coffee Wrangler','2013-09-09','3');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (10,'Ima','Winifred','Coffee Wrangler','2016-12-10','3');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (11,'Ruth','Leslie','Store Manager','2009-06-17','4');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (12,'Britanni','Jorden','Coffee Wrangler','2006-03-25','4');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (13,'Berk','Derek','Coffee Wrangler','2009-12-11','4');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (14,'Damon','Sasha','Coffee Wrangler','2010-06-05','4');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (15,'Remedios','Mari','Coffee Wrangler','2014-05-09','4');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (16,'Reed','Eve','Store Manager','2006-03-30','5');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (17,'Quail','Octavia','Coffee Wrangler','2014-12-05','5');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (18,'Ezekiel','Rashad','Coffee Wrangler','2005-11-13','5');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (19,'Peter','Paloma','Coffee Wrangler','2014-03-12','5');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (20,'Ronan','Magee','Coffee Wrangler','2002-02-13','5');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (21,'Melodie','Mercedes','Store Manager','2018-09-29','6');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (22,'Marny','Dennis','Coffee Wrangler','2014-03-03','6');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (23,'Blythe','Arsenio','Coffee Wrangler','2018-11-22','6');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (24,'Garrett','Doris','Coffee Wrangler','2007-01-27','6');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (25,'Aline','Melanie','Coffee Wrangler','2017-03-14','6');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (26,'Joelle','Christen','Store Manager','2013-11-11','7');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (27,'Ainsley','Evelyn','Coffee Wrangler','2003-07-31','7');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (28,'Joseph','Byron','Coffee Wrangler','2014-05-17','7');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (29,'Orson','Benedict','Coffee Wrangler','2016-07-26','7');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (30,'Amela','Chadwick','Coffee Wrangler','2005-09-17','7');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (31,'Dawn','Anthony','Store Manager','2009-07-02','8');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (32,'Alisa','Lysandra','Coffee Wrangler','2005-02-02','8');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (33,'Cairo','Vaughan','Coffee Wrangler','2015-02-18','8');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (34,'Yasir','Lillith','Coffee Wrangler','2016-02-16','8');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (35,'Xavier','Zachary','Coffee Wrangler','2014-01-07','8');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (36,'Anthony','Kaitlin','Store Manager','2004-04-19','9');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (37,'Hop','Bianca','Coffee Wrangler','2015-02-11','9');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (38,'Ezekiel','Bertha','Coffee Wrangler','2008-10-15','9');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (39,'Vance','Samuel','Coffee Wrangler','2016-06-09','9');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (40,'Brent','Herman','Coffee Wrangler','2001-10-25','9');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (41,'Adrian','Macon','Store Manager','2001-10-13','10');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (42,'Kylie','Candace','Coffee Wrangler','2011-01-30','10');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (43,'Tatum','Laurel','Coffee Wrangler','2015-01-31','10');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (44,'Tamekah','Maya','Coffee Wrangler','2005-05-17','10');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (45,'Pandora','Neville','Coffee Wrangler','2019-03-21','10');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (46,'Desiree','Anika','Store Manager','2008-06-15','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (47,'Hope','Sheila','Coffee Wrangler','2003-01-31','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (48,'Clare','Oscar','Coffee Wrangler','2003-02-21','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (49,'Gemma','Eaton','Coffee Wrangler','2013-05-04','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (50,'Dale','Joshua','Coffee Wrangler','2015-10-01','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (51,'Lawrence','Roberts','Store Manager','2001-08-03','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (52,'Melinda','Zeus','Coffee Wrangler','2003-08-17','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (53,'Sawyer','Kasper','Coffee Wrangler','2007-11-11','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (54,'Ezekiel','Griffin','Coffee Wrangler','2005-09-13','FL');
+INSERT INTO staffs(staff_id,first_name,last_name,position,start_date,location) VALUES (55,'Coby','Shelly','Coffee Wrangler','2015-11-17','FL');
